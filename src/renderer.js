e44370191924050c7443fa64e3af391f2b642980
Felix <felix.uellendall@gmx.de>
2017-09-10 12:07:45 +0200

    refactoring

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,14 @@
 import { GitHubCrawler } from './github/crawler/github-crawler';
 
-const init = async() => {
+const init = async () => {
     const gitHubCrawler = new GitHubCrawler();
-    const poeRepos = gitHubCrawler.getPoERepos();
+    const poeRepos = await gitHubCrawler.getPoERepos();
 
     return poeRepos;
 };
 
 const render = (poeRepos) => {
-    var app = new Vue({
+    const app = new Vue({
         el: '#app',
         data: {
             message: poeRepos
